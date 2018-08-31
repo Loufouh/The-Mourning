8a32e5f5f9ee83ed7339323145532dfec1438017
Louis <louis.masson@palissy.fr>
2018-08-31 19:28:42 +0200

    Add other missing files and folders

--- inc/initialize.js
+++ inc/initialize.js
@@ -1,7 +1,23 @@
 "use strict";
 
+let canvas;
+
 window.onload = init;
 
 function init() {
+	initCanvas();
+	background(52);
+}
 
+function initCanvas() {
+	canvas = document.querySelector("#gameCanvas");
+	setTargetContext(canvas.getContext("2d"));
+
+	if(window.innerHeight < window.innerWidth) {
+		canvas.height = Math.floor(7*window.innerHeight/8);
+		canvas.width = canvas.height*1.618;
+	} else {
+		canvas.width = Math.floor(7*window.innerWidth/8);
+		canvas.height = canvas.width/1.618;
+	}
 }
