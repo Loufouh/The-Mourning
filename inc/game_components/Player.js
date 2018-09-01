91a1def65b0056cab9e025345658689124b0e79f
Louis Masson <louis.masson@palissy.fr>
2018-09-01 19:57:40 +0200

    Slow side walk animation

--- inc/game_components/Player.js
+++ inc/game_components/Player.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const WSS = 5; // walking sprite speed
+const WSSF = Math.floor(WSS*8/6);
 
 const player_states = {
 	IDLE_UP: "0",
@@ -109,43 +110,43 @@ class Player extends Character {
 		switch(this.state) {
 			case player_states.IDLE_UP:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkUp, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0], [1], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0], [1], new Vector(this.dim.x, this.dim.y));
 				break;
 			case  player_states.IDLE_DOWN:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkDown, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0], [1], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0], [1], new Vector(this.dim.x, this.dim.y));
 				break;
 			case  player_states.IDLE_LEFT:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkLeft, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0], [1], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0], [1], new Vector(this.dim.x, this.dim.y));
 				break;
 			case player_states.IDLE_RIGHT:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkRight, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0], [1], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0], [1], new Vector(this.dim.x, this.dim.y));
 				break;
 			case player_states.WALK_UP:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkUp, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0, 1, 2, 3, 4, 5, 6, 7], [WSS, WSS, WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0, 1, 2, 3, 4, 5, 6, 7], [WSS, WSS, WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
 				break;
 			case player_states.WALK_DOWN:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkDown, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0, 1, 2, 3, 4, 5, 6, 7], [WSS, WSS, WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0, 1, 2, 3, 4, 5, 6, 7], [WSS, WSS, WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
 				break;
 			case player_states.WALK_LEFT:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkLeft, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0, 1, 2, 3, 4, 5], [WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0, 1, 2, 3, 4, 5], [WSSF, WSSF, WSSF, WSSF, WSSF, WSSF], new Vector(this.dim.x, this.dim.y));
 				break;
 			case player_states.WALK_RIGHT:
 				this.animation = new Animation(new SpriteSheet(this.spriteSheets.walkRight, new Vector(32, 32), 5),
-											   new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
-											   [0, 1, 2, 3, 4, 5], [WSS, WSS, WSS, WSS, WSS, WSS], new Vector(this.dim.x, this.dim.y));
+							       new Vector(canvas.width/2 - this.dim.x/2, canvas.height/2 - this.dim.y/2),
+							       [0, 1, 2, 3, 4, 5], [WSSF, WSSF, WSSF, WSSF, WSSF, WSSF], new Vector(this.dim.x, this.dim.y));
 				break;
 		}
 	}
