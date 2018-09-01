9b6f06ca3dac86c2d31c9e5523c820c0d812e9d1
Louis Masson <louis.masson@palissy.fr>
2018-09-02 01:44:53 +0200

    Add some effects to make an atmosphere

--- inc/sketch.js
+++ inc/sketch.js
@@ -3,17 +3,29 @@
 let player;
 let map;
 
+let body;
+let metallicMonksAudio;
+let thunderAudio;
+
+let isNight = true;
+
 function setup() {
+	body = document.querySelector("body");
+	metallicMonksAudio = document.querySelector("#metallicMonksAudio");
+	thunderAudio = document.querySelector("#thunderAudio");
+
 	player = new Player(new Vector(canvas.width/2 - 25, canvas.height/2 - 25),
-			    new Vector(150, 150),
-			    new Color(100, 100, 255));
+			    new Vector(150, 150));
 	map = new GameMap(new Vector(2000, 2000), new Color(52, 100, 52));
 
 	for(let i = 0; i < 125; i++) {
 		map.addContent( new GameObject(new Vector(randomInt(0, map.dim.x - 25), randomInt(0, map.dim.y - 25)), 
-					       new Vector(25, 25), 
-					       new Color(255, 255, 100)) );
+					       			   new Vector(25, 25), 
+									   new Color(255, 255, 100)) );
 	}
+	setTimeout(enableThumb, randomInt(1000, 10000));
+	body.classList.add("night");
+	metallicMonksAudio.play();
 }
 
 function loop() {
@@ -23,4 +35,22 @@ function loop() {
 
 	map.draw();
 	player.draw();
+
+	if(isNight)
+		background(new Color(0, 0, 100, 0.5))
+}
+
+function enableThumb() {
+	toogleThumb();
+	setTimeout(enableThumb, randomInt(1000, 10000));
+}
+
+function toogleThumb() {
+	let body = document.querySelector("body");
+
+	body.classList.add("thumb");
+	thunderAudio.play();
+
+	setTimeout(()=>thunderAudio.pause(), 3980);
+	setTimeout(()=>body.classList.remove("thumb"), 5000);
 }
